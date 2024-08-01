5c50cdeb646292f9d9c24617670466ea74055275
Edwin <oriokie@icloud.com>
2024-08-01 13:06:44 +0300

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+The lock boxes puzzle
+"""
+
+
+def canUnlockAll(boxes):
+    n = len(boxes)
+    opened = set()
+    queue = [0]
+
+    while queue:
+        box = queue.pop(0)
+        if box not in opened:
+            opened.add(box)
+            for key in boxes[box]:
+                if key < n and key not in opened:
+                    queue.append(key)
+
+    return len(opened) == n
