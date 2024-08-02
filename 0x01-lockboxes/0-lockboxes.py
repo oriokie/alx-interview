d3754ee76be7805e7c7d3c6fc528dbc4171a6ccb
Edwin <oriokie@icloud.com>
2024-08-02 11:39:09 +0300

    changed the queues to be a set

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,14 +10,14 @@ def canUnlockAll(boxes):
     """
     n = len(boxes)
     opened = set()
-    queue = [0]
+    queue = set([0])
 
     while queue:
-        box = queue.pop(0)
+        box = queue.pop()
         if box not in opened:
             opened.add(box)
             for key in boxes[box]:
                 if key < n and key not in opened:
-                    queue.append(key)
+                    queue.add(key)
 
     return len(opened) == n
