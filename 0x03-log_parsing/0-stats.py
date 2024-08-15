94bad08ad4d1abe9cf196d8f6e8f6c2ea885a1ef
Edwin <oriokie@icloud.com>
2024-08-15 11:16:22 +0300

    done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -25,11 +25,12 @@ def parse_line(line: str) -> Optional[Tuple[int, int]]:
     parses a line to extract status code and file size"
     """
     try:
-        ip, _, _, datetime, request, status_code, file_size = line.split(' ', 6)
-        if request != '"GET /projects/260 HTTP/1.1"':
+        parts = line.split()
+
+        if len(parts) < 7:
             return None
-        status_code = int(status_code)
-        file_size = int(file_size)
+        status_code = int(parts[-2])
+        file_size = int(parts[-1])
         return status_code, file_size
     except (IndexError, ValueError):
         return None
