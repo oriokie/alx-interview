d974b6db41ad2f75e5070b18a1603b923ecc1685
Edwin <oriokie@icloud.com>
2024-09-23 10:29:49 +0300

    added the main file

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -17,19 +17,20 @@ def island_perimeter(grid):
         return 0
 
     def dfs(i, j):
-        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0:
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(
+                grid[0]) or grid[i][j] == 0:
             return 1  # Water edge, contribute to perimeter
         if (i, j) in visited:
             return 0  # Already visited land, don't contribute to perimeter
-        
+
         visited.add((i, j))
-        
+
         perimeter = 0
-        perimeter += dfs(i-1, j)  # Up
-        perimeter += dfs(i+1, j)  # Down
-        perimeter += dfs(i, j-1)  # Left
-        perimeter += dfs(i, j+1)  # Right
-        
+        perimeter += dfs(i - 1, j)  # Up
+        perimeter += dfs(i + 1, j)  # Down
+        perimeter += dfs(i, j - 1)  # Left
+        perimeter += dfs(i, j + 1)  # Right
+
         return perimeter
 
     visited = set()
@@ -37,5 +38,5 @@ def island_perimeter(grid):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
                 return dfs(i, j)  # Start DFS from the first land cell found
-    
+
     return 0  # No land found
